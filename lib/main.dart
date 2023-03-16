24f575ef33354059830f25b5b2a385ae9c09e5a7
하대훈 <ggrrm@hadaehun-ui-MacBookAir.local>
2023-03-16 22:43:48 +0900

    5.0 Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -8,6 +7,33 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const Scaffold(
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
+
+/*
+class Cardwid extends StatelessWidget {
+  const Cardwid({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -146,3 +172,4 @@ class App extends StatelessWidget {
     );
   }
 }
+*/
